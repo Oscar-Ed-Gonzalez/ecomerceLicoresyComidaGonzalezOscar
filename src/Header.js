57d0b2fa028457aeb03bd2271a6b988a7f93f004
Oscar Saavedra <osegos10@gmail.com>
2021-07-26 14:21:57 -0500

    App menu

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+const Header = () =>
+    <header>
+        <h1>E-COMMERCE</h1>
+        <nav>
+            <a href="#"> Iniciar Sesión</a>
+            <a href="#"> Mis pedidos </a>
+            <a href="#"> Carrito De Compras</a>
+        </nav>
+    </header> 
+
+export default Header
